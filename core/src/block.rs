417a10e88054e3de840896a9f793c1ea8d3a704c
Frederik Schöll <fschoell@users.noreply.github.com>
2022-11-29 18:50:20 +0100

    add a helper function to get all executed transaction traces

--- core/src/block.rs
+++ core/src/block.rs
@@ -1,14 +1,20 @@
 use crate::{pb::antelope as pb};
+use crate::block::pb::TransactionStatus::TransactionstatusExecuted;
 
 impl pb::Block {
-
     /// returns all transaction traces from the block.
-    pub fn all_transaction_traces(&self) -> impl Iterator<Item = &pb::TransactionTrace> {
+    pub fn all_transaction_traces(&self) -> impl Iterator<Item=&pb::TransactionTrace> {
         return if self.filtering_applied == true {
             self.filtered_transaction_traces.iter()
         } else {
             self.unfiltered_transaction_traces.iter()
-        }
+        };
+    }
+
+    /// returns all transaction traces which have the status `executed`
+    pub fn executed_transaction_traces(&self) -> impl Iterator<Item=&pb::TransactionTrace> {
+        return self.all_transaction_traces()
+            .filter(|trx| trx.receipt.as_ref().unwrap().status == TransactionstatusExecuted as i32);
     }
 
     /// returns the number of transaction traces included in this block
@@ -17,7 +23,6 @@ impl pb::Block {
             self.filtered_transaction_count
         } else {
             self.unfiltered_transaction_count
-        }
+        };
     }
-
 }
