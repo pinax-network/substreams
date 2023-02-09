f7fba6f782c74f976288208e81a915c70c925838
Denis Carriere <carriere.denis@gmail.com>
2023-02-08 20:35:53 -0500

    add custom params

--- params/src/lib.rs
+++ params/src/lib.rs
@@ -5,5 +5,6 @@ use substreams::pb::substreams::Clock;
 #[substreams::handlers::map]
 pub fn map_params(params: String, clock: Clock) -> Result<Clock, Error> {
     log::debug!("map_params: {:?}", params);
+    log::debug!("2nd log map_params: {:?}", params);
     Ok(clock)
 }
