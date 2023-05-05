f59b1d41f084466a7ae7279ca41ad541ba971bc7
YaroShkvorets <shkvorets@gmail.com>
2023-05-05 11:24:20 -0400

    bump up antelope version, fix clones

--- eosio.token/src/maps.rs
+++ eosio.token/src/maps.rs
@@ -12,7 +12,7 @@ fn map_accounts(block: Block) -> Result<Accounts, Error> {
         for db_op in &trx.db_ops {
             if db_op.table_name != "accounts" { continue; }
 
-            let contract = db_op.clone().code;
+            let contract = db_op.code.clone();
             let raw_primary_key = Name::from(db_op.primary_key.as_str()).value;
             let symcode = SymbolCode::from(raw_primary_key).to_string();
             let account = db_op.scope.clone();
@@ -41,7 +41,7 @@ fn map_stat(block: Block) -> Result<Stats, Error> {
         for db_op in &trx.db_ops {
             if db_op.table_name != "stat" { continue; }
 
-            let contract = db_op.clone().code;
+            let contract = db_op.code.clone();
             let raw_primary_key = Name::from(db_op.primary_key.as_str()).value;
             let symcode = SymbolCode::from(raw_primary_key).to_string();
             let currency_stats = abi::CurrencyStats::try_from(db_op.new_data_json.as_str());
@@ -70,28 +70,27 @@ fn map_transfers(block: Block) -> Result<TransferEvents, Error> {
     for trx in block.all_transaction_traces() {
         // action traces
         for trace in &trx.action_traces {
-            let action_trace = trace.action.as_ref().unwrap().clone();
+            let action_trace = trace.action.as_ref().unwrap();
             if action_trace.account != trace.receiver { continue; }
             if action_trace.name != "transfer"  { continue; }
-            let transfer = abi::Transfer::try_from(action_trace.json_data.as_str());
 
-            match (transfer) {
-                Ok(data) => {
-                    let quantity = Asset::from(data.quantity.as_str());
+            match abi::Transfer::try_from(action_trace.json_data.as_str()) {
+                Ok(transfer) => {
+                    let quantity = Asset::from(transfer.quantity.as_str());
                     response.push(TransferEvent {
                         // trace information
                         trx_id: trx.id.clone(),
                         action_ordinal: trace.action_ordinal,
 
                         // contract & scope
-                        account: action_trace.account,
+                        account: action_trace.account.clone(),
                         symcode: quantity.symbol.code().to_string(),
 
                         // payload
-                        from: data.from,
-                        to: data.to,
+                        from: transfer.from,
+                        to: transfer.to,
                         quantity: quantity.to_string(),
-                        memo: data.memo,
+                        memo: transfer.memo,
 
                         // extras
                         precision: quantity.symbol.precision().into(),
