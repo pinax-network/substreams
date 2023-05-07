9f827b5323a51a361d59bc6559edbb8adb71ee81
YaroShkvorets <shkvorets@gmail.com>
2023-05-06 21:02:30 -0400

    replace str to char

--- eosio.token/src/utils.rs
+++ eosio.token/src/utils.rs
@@ -9,12 +9,12 @@ use substreams_database_change::change::AsString;
 // all EOS symcode transfers to these two accounts
 pub fn create_filters(params: &str, key: &str) -> HashSet<String> {
     let mut filter = HashSet::new();
-    for part in params.split("&").collect::<Vec<&str>>() {
-        let kv = part.split("=").collect::<Vec<&str>>();
+    for part in params.split('&').collect::<Vec<&str>>() {
+        let kv = part.split('=').collect::<Vec<&str>>();
         if ( kv.len() != 2 ) || ( kv[0] != key ) {
             continue;
         }
-        for item in kv[1].split(",").collect::<Vec<&str>>() {
+        for item in kv[1].split(',').collect::<Vec<&str>>() {
             filter.insert(item.as_string());
         }
     }
