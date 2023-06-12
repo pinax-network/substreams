d640a397fd187a1c951ccea322bff65d5995f04d
jhjhjh94 <hau@pinax.network>
2023-06-12 17:04:40 +0800

    Updated structures for eosio.evm

--- eosio.evm/src/maps.rs
+++ eosio.evm/src/maps.rs
@@ -2,6 +2,11 @@ use substreams::errors::Error;
 use substreams_antelope::Block;
 use substreams::log;
 
+use antelope::{Asset, Name, SymbolCode};
+
+//use serde_json::Result;
+use serde_json::Value;
+
 use crate::eosio_evm;
 use crate::abi;
 
@@ -25,15 +30,39 @@ fn map_pushtx(block: Block) -> Result<eosio_evm::Pushtxs, Error> {
                 Err(_) => continue,
             }
         }
-
-        for db_op in &trx.db_ops {
-            let contract = db_op.code.clone();
-    
-            if contract == "eosio.evm" &&  db_op.table_name == "balances" {
-                log::debug!("new_data_json={:?}", db_op.new_data_json);
-            }
-        }
     }
 
     Ok(eosio_evm::Pushtxs { events })
+}
+
+#[substreams::handlers::map]
+fn map_balances(block: Block) -> Result<eosio_evm::Pushbalances, Error> {
+    let mut events = vec![];
+    for trx in block.all_transaction_traces() {
+        for db_op in &trx.db_ops {
+            let contract = db_op.code.clone();
+
+            if contract == "eosio.evm" &&  db_op.table_name == "balances" {
+                log::debug!("new_data_json={:?}", db_op.new_data_json);
+                
+                match serde_json::from_str::<Value>(&db_op.new_data_json) {
+                    Ok(parsed_value) => {
+                        log::debug!("Parsed JSON: {:?}", parsed_value);
+
+                        events.push(eosio_evm::Pushbalance {
+                            miner: "testing12345".to_string(),
+                            balance: 1.0001,
+                            symcode: "EOS".to_string(),
+                        });
+
+                    }
+                    Err(e) => {
+                        log::debug!("Failed to parse JSON: {}", e);
+                    }
+                }
+
+            }
+       }
+    }
+    Ok(eosio_evm::Pushbalances { events })
 }
