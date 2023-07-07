3fd9011980e60aab6d3e0a5f9ec0aad9cee31cc7
YaroShkvorets <shkvorets@gmail.com>
2023-07-06 23:40:09 -0400

    remove expensive clones

--- atomicmarket/src/maps.rs
+++ atomicmarket/src/maps.rs
@@ -5,7 +5,7 @@ use substreams_antelope::{Block, ActionTraces};
 fn map_actions(block: Block) -> Result<ActionTraces, Error> {
     let mut action_traces = vec![];
 
-    for trx in block.clone().all_transaction_traces() {
+    for trx in block.all_transaction_traces() {
         for trace in &trx.action_traces {
             let action_trace = trace.action.as_ref().unwrap().clone();
             if action_trace.account != "atomicmarket" { continue; }
