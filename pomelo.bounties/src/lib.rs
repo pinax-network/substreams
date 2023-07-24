178111f3a294b829c0fe184fecce64f9e2ca38d7
YaroShkvorets <shkvorets@gmail.com>
2023-07-24 13:10:50 -0400

    refactor module

--- pomelo.bounties/src/lib.rs
+++ pomelo.bounties/src/lib.rs
@@ -7,35 +7,29 @@ use pb::antelope::pomelo::bounties::v1::{Actions, StateLog};
 
 const TRACKED_CONTRACT: &str = "d.w.pomelo";
 
-
 #[substreams::handlers::map]
 fn map_actions(block: Block) -> Result<Actions, substreams::errors::Error> {
-    let mut actions = Actions::default();
+    Ok(Actions {
+        statelogs: block.all_transaction_traces()
+            .flat_map(|trx| trx.action_traces.iter())
+            .filter(|trace| trace.action.as_ref().unwrap().account == TRACKED_CONTRACT)
+            .filter(|trace| trace.action.as_ref().unwrap().name == "statelog")
+            .map(|trace| {
+                match abi::Statelog::try_from(trace.action.as_ref().unwrap().json_data.as_str()) {
+                    Ok(data) =>
+                        StateLog {
+                            // trx information
+                            trx_id: trace.transaction_id.clone(),
+                            trx_index: trace.action_ordinal,
 
-    for trx in block.all_transaction_traces() {
-        // action traces
-        for trace in &trx.action_traces {
-            let action_trace = trace.action.as_ref().unwrap();
-            if action_trace.account != trace.receiver { continue; }
-            if action_trace.account != TRACKED_CONTRACT { continue; }
-            if action_trace.name != "statelog" { continue; }
-
-            match abi::Statelog::try_from(action_trace.json_data.as_str()) {
-                Ok(data) => {
-                    actions.statelogs.push(StateLog {
-                        // trace information
-                        trx_id: trx.id.clone(),
-                        trx_index: trace.action_ordinal,
-
-                        // action data
-                        action: data.action,
-                        state: data.status,
-                        bounty_id: data.bounty_id,
-                    });
+                            // action data
+                            action: data.action,
+                            state: data.status,
+                            bounty_id: data.bounty_id,
+                        },
+                    Err(_) => panic!("Error parsing statelog action data"),
                 }
-                Err(_) => panic!("Error parsing action data"),
-            }
-        }
-    }
-    Ok(actions)
+            })
+            .collect(),
+    })
 }
