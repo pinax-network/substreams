0fa4f3fb03b5ea19ff6b1706cdb118a1e0a4c0c5
jhjhjh94 <hau@pinax.network>
2023-08-29 17:01:53 +0800

    eosio.cpu prom_out developed

--- eosio.cpu/src/maps.rs
+++ eosio.cpu/src/maps.rs
@@ -8,200 +8,6 @@ use crate::eosio_cpu::*;
 use crate::utils;
 use antelope::{Asset, Name, SymbolCode};
 
-/*
-#[substreams::handlers::map]
-fn map_accounts(params: String, block: Block) -> Result<Accounts, Error> {
-    let mut items = vec![];
-
-    // query-params
-    let filter_account = utils::create_filters(params.as_str(), "account");
-    let filter_symcode = utils::create_filters(params.as_str(), "symcode");
-    let filter_contract = utils::create_filters(params.as_str(), "contract");
-
-    let filter_balance_lt = utils::create_i64_filter(params.as_str(), "balance_lt");
-    let filter_balance_gt = utils::create_i64_filter(params.as_str(), "balance_gt");
-    let filter_balance_lte = utils::create_i64_filter(params.as_str(), "balance_lte");
-    let filter_balance_gte = utils::create_i64_filter(params.as_str(), "balance_gte");
-
-    let filter_balance_delta_lt = utils::create_i64_filter(params.as_str(), "balance_delta_lt");
-    let filter_balance_delta_gt = utils::create_i64_filter(params.as_str(), "balance_delta_gt");
-    let filter_balance_delta_lte = utils::create_i64_filter(params.as_str(), "balance_delta_lte");
-    let filter_balance_delta_gte = utils::create_i64_filter(params.as_str(), "balance_delta_gte");
-
-    for trx in block.all_transaction_traces() {
-        for db_op in &trx.db_ops {
-            if db_op.table_name != "accounts" { continue; }
-
-            let contract = db_op.code.clone();
-            let raw_primary_key = Name::from(db_op.primary_key.as_str()).value;
-            let symcode = SymbolCode::from(raw_primary_key);
-            let account = db_op.scope.clone();
-
-            // filter by params
-            if !filter_account.is_empty() && !filter_account.contains(&account) { continue; }
-            if !filter_symcode.is_empty() && !filter_symcode.contains(&symcode.to_string()) { continue; }
-            if !filter_contract.is_empty() && !filter_contract.contains(&contract) { continue; }
-
-            let old_data = abi::Account::try_from(db_op.old_data_json.as_str()).ok();
-            let new_data = abi::Account::try_from(db_op.new_data_json.as_str()).ok();
-            if old_data.is_none() && new_data.is_none() { continue; } // no data
-
-            let old_balance = match &old_data {
-                Some(data) => Some(Asset::from(data.balance.as_str())),
-                None => None,
-            };
-            let new_balance = match &new_data {
-                Some(data) => Some(Asset::from(data.balance.as_str())),
-                None => None,
-            };
-            let precision = match new_balance.is_some() {
-                true => new_balance.unwrap().symbol.precision(),
-                false => old_balance.unwrap().symbol.precision(),
-            };
-            let sym = Symbol::from_precision(symcode, precision);
-            let balance = match new_balance.is_some() {
-                true => new_balance.unwrap(),
-                false => Asset::from_amount(0, sym),
-            };
-
-            let balance_delta = match old_balance.is_some() {
-                true => balance.amount - old_balance.unwrap().amount,
-                false => balance.amount,
-            };
-
-            // filter by params
-            if filter_balance_lt.is_some() && !(balance.amount < filter_balance_lt.unwrap()) { continue; }
-            if filter_balance_gt.is_some() && !(balance.amount > filter_balance_gt.unwrap()) { continue; }
-            if filter_balance_lte.is_some() && !(balance.amount <= filter_balance_lte.unwrap()) { continue; }
-            if filter_balance_gte.is_some() && !(balance.amount >= filter_balance_gte.unwrap()) { continue; }
-
-            if filter_balance_delta_lt.is_some() && !(balance_delta < filter_balance_delta_lt.unwrap()) { continue; }
-            if filter_balance_delta_gt.is_some() && !(balance_delta > filter_balance_delta_gt.unwrap()) { continue; }
-            if filter_balance_delta_lte.is_some() && !(balance_delta <= filter_balance_delta_lte.unwrap()) { continue; }
-            if filter_balance_delta_gte.is_some() && !(balance_delta >= filter_balance_delta_gte.unwrap()) { continue; }
-
-            items.push(Account {
-                // trace information
-                trx_id: trx.id.clone(),
-                action_index: db_op.action_index,
-
-                // contract & scope
-                contract,
-                symcode: symcode.to_string(),
-
-                // payload
-                account: account,
-                balance: balance.to_string(),
-                balance_delta,
-
-                // extras
-                precision: precision.into(),
-                amount: balance.amount,
-                value: utils::to_value(balance),
-            });
-        }
-    }
-    Ok(Accounts { items })
-}
-
-#[substreams::handlers::map]
-fn map_stat(params: String, block: Block) -> Result<Stats, Error> {
-    let mut items = vec![];
-
-    // query-params
-    let filter_symcode = utils::create_filters(params.as_str(), "symcode");
-    let filter_contract = utils::create_filters(params.as_str(), "contract");
-
-    let filter_supply_lt = utils::create_i64_filter(params.as_str(), "supply_lt");
-    let filter_supply_gt = utils::create_i64_filter(params.as_str(), "supply_gt");
-    let filter_supply_lte = utils::create_i64_filter(params.as_str(), "supply_lte");
-    let filter_supply_gte = utils::create_i64_filter(params.as_str(), "supply_gte");
-
-    let filter_supply_delta_lt = utils::create_i64_filter(params.as_str(), "supply_delta_lt");
-    let filter_supply_delta_gt = utils::create_i64_filter(params.as_str(), "supply_delta_gt");
-    let filter_supply_delta_lte = utils::create_i64_filter(params.as_str(), "supply_delta_lte");
-    let filter_supply_delta_gte = utils::create_i64_filter(params.as_str(), "supply_delta_gte");
-
-    for trx in block.all_transaction_traces() {
-        for db_op in &trx.db_ops {
-            if db_op.table_name != "stat" { continue; }
-
-            let contract = db_op.code.clone();
-            let raw_primary_key = Name::from(db_op.primary_key.as_str()).value;
-            let symcode = SymbolCode::from(raw_primary_key);
-
-            // filter by params
-            if !filter_contract.is_empty() && !filter_contract.contains(&contract) { continue; }
-            if !filter_symcode.is_empty() && !filter_symcode.contains(&symcode.to_string()) { continue; }
-            if !filter_contract.is_empty() && !filter_contract.contains(&contract) { continue; }
-
-            let old_data = abi::CurrencyStats::try_from(db_op.old_data_json.as_str()).ok();
-            let new_data = abi::CurrencyStats::try_from(db_op.new_data_json.as_str()).ok();
-            if old_data.is_none() && new_data.is_none() { continue; } // no data
-
-            let old_supply = match &old_data {
-                Some(data) => Some(Asset::from(data.supply.as_str())),
-                None => None,
-            };
-            let new_supply = match &new_data {
-                Some(data) => Some(Asset::from(data.supply.as_str())),
-                None => None,
-            };
-            let precision = match new_supply.is_some() {
-                true => new_supply.unwrap().symbol.precision(),
-                false => old_supply.unwrap().symbol.precision(),
-            };
-            let sym = Symbol::from_precision(symcode, precision);
-            let supply = match new_supply.is_some() {
-                true => new_supply.unwrap(),
-                false => Asset::from_amount(0, sym),
-            };
-
-            let supply_delta = match old_supply.is_some() {
-                true => supply.amount - old_supply.unwrap().amount,
-                false => supply.amount,
-            };
-
-            // filter by params
-            if filter_supply_lt.is_some() && !(supply.amount < filter_supply_lt.unwrap()) { continue; }
-            if filter_supply_gt.is_some() && !(supply.amount > filter_supply_gt.unwrap()) { continue; }
-            if filter_supply_lte.is_some() && !(supply.amount <= filter_supply_lte.unwrap()) { continue; }
-            if filter_supply_gte.is_some() && !(supply.amount >= filter_supply_gte.unwrap()) { continue; }
-
-            if filter_supply_delta_lt.is_some() && !(supply_delta < filter_supply_delta_lt.unwrap()) { continue; }
-            if filter_supply_delta_gt.is_some() && !(supply_delta > filter_supply_delta_gt.unwrap()) { continue; }
-            if filter_supply_delta_lte.is_some() && !(supply_delta <= filter_supply_delta_lte.unwrap()) { continue; }
-            if filter_supply_delta_gte.is_some() && !(supply_delta >= filter_supply_delta_gte.unwrap()) { continue; }
-
-            // TO-DO fix in case of new_data is None
-            let data = new_data.unwrap();
-
-            items.push(Stat {
-                // trace information
-                trx_id: trx.id.clone(),
-                action_index: db_op.action_index,
-
-                // contract & scope
-                contract,
-                symcode: symcode.to_string(),
-
-                // payload
-                issuer: data.issuer,
-                max_supply: data.max_supply,
-                supply: supply.to_string(),
-                supply_delta,
-
-                // extras
-                precision: precision.into(),
-                amount: supply.amount,
-                value: utils::to_value(supply),
-            });
-        }
-    }
-    Ok(Stats { items })
-}
-*/
-
 #[substreams::handlers::map]
 fn map_transfers(params: String, block: Block) -> Result<TransferEvents, Error> {
     let mut response = vec![];
@@ -236,8 +42,8 @@ fn map_transfers(params: String, block: Block) -> Result<TransferEvents, Error> 
                 Ok(data) => {
                     let quantity = Asset::from(data.quantity.as_str());
                     let symcode = quantity.symbol.code().to_string();
-                    let precision = quantity.symbol.precision().into();
-                    let amount = quantity.amount;
+                    //let precision = quantity.symbol.precision().into();
+                    //let amount = quantity.amount;
                     let contract = action_trace.account.clone();
 
                     // filter by params
@@ -246,17 +52,17 @@ fn map_transfers(params: String, block: Block) -> Result<TransferEvents, Error> 
                     if !filter_symcode.is_empty() && !filter_symcode.contains(&symcode) { continue; }
                     if !filter_contract.is_empty() && !filter_contract.contains(&contract) { continue; }
                     if !filter_to_or_from.is_empty() && !(filter_to_or_from.contains(&data.to) || filter_to_or_from.contains(&data.from)) { continue; }
-                    if filter_quantity_lt.is_some() && !(quantity.amount < filter_quantity_lt.unwrap()) { continue; }
-                    if filter_quantity_gt.is_some() && !(quantity.amount > filter_quantity_gt.unwrap()) { continue; }
-                    if filter_quantity_lte.is_some() && !(quantity.amount <= filter_quantity_lte.unwrap()) { continue; }
-                    if filter_quantity_gte.is_some() && !(quantity.amount >= filter_quantity_gte.unwrap()) { continue; }
+                    //if filter_quantity_lt.is_some() && !(quantity.amount < filter_quantity_lt.unwrap()) { continue; }
+                    //if filter_quantity_gt.is_some() && !(quantity.amount > filter_quantity_gt.unwrap()) { continue; }
+                    //if filter_quantity_lte.is_some() && !(quantity.amount <= filter_quantity_lte.unwrap()) { continue; }
+                    //if filter_quantity_gte.is_some() && !(quantity.amount >= filter_quantity_gte.unwrap()) { continue; }
 
                     transaction_count += 1;
 
                     response.push(TransferEvent {
                         // trace information
                         trx_id: trx.id.clone(),
-                        action_ordinal: trace.action_ordinal,
+                        //action_ordinal: trace.action_ordinal,
 
                         // contract & scope
                         contract,
@@ -270,9 +76,9 @@ fn map_transfers(params: String, block: Block) -> Result<TransferEvents, Error> 
                         memo: data.memo,
 
                         // extras
-                        precision,
-                        amount,
-                        value: utils::to_value(quantity),
+                        //precision,
+                        //amount,
+                        //value: utils::to_value(quantity),
 
                         // cpu
                         producer: producer.to_string(),
--- eosio.cpu/src/sinks.rs
+++ eosio.cpu/src/sinks.rs
@@ -1,99 +1,35 @@
 use antelope::Asset;
+use std::collections::HashMap;
 use substreams::errors::Error;
 use substreams_entity_change::pb::entity::{entity_change, EntityChanges};
 use substreams_sink_kv::pb::sf::substreams::sink::kv::v1::KvOperations;
 use substreams_database_change::pb::database::{table_change, DatabaseChanges};
+use substreams_sink_prometheus::{PrometheusOperations, Counter, Gauge, Histogram, Summary};
+use substreams::log;
 
 use crate::eosio_cpu::{Accounts, Stats, TransferEvents};
 
 #[substreams::handlers::map]
-pub fn graph_out(map_transfers: TransferEvents) -> Result<EntityChanges, Error> {
-    let mut entity_changes: EntityChanges = Default::default();
+pub fn prom_out(map_transfers: TransferEvents) -> Result<PrometheusOperations, Error> {
+
+    let mut prom_out = PrometheusOperations::default();
+    //log::info!("map_transfers={:?}", map_transfers);
 
     for transfer in map_transfers.items {
-        entity_changes
-            .push_change("transfer", transfer.trx_id.as_str(), 0, entity_change::Operation::Create)
+        log::info!("producer={:?}", transfer.producer);
+        log::info!("cpu_usage={:?}", transfer.cpu_usage);
+        log::info!("net_usage={:?}", transfer.net_usage);
+        log::info!("tx_count={:?}", transfer.tx_count);
+        log::info!("from={:?}", transfer.from);
 
-            // transaction
-            .change("trx_id", transfer.trx_id)
-            .change("action_ordinal", transfer.action_ordinal.to_string())
+        let transfer_label = HashMap::from([
+            ("producer".to_string(), transfer.producer.to_string()),
+            ("from".to_string(), transfer.from.to_string())
+        ]);
 
-            // contract & scope
-            .change("contract", transfer.contract)
-            .change("action", transfer.action)
-            .change("symcode", transfer.symcode)
-
-            // data payload
-            .change("from", transfer.from)
-            .change("to", transfer.to)
-            .change("memo", transfer.memo)
-            .change("quantity", transfer.quantity)
-
-            // extras
-            .change("amount", transfer.amount.to_string())
-            .change("precision", transfer.precision.to_string())
-            .change("value", transfer.value.to_string());
-            
+        prom_out.push(Gauge::from("cpu_usage").with(transfer_label.clone()).set(transfer.cpu_usage as f64));
+        prom_out.push(Gauge::from("net_usage").with(transfer_label.clone()).set(transfer.net_usage as f64));
+        prom_out.push(Gauge::from("tx_count").with(transfer_label.clone()).set(transfer.tx_count as f64));
     }
-
-    Ok(entity_changes)
-}
-
-/*
-#[substreams::handlers::map]
-pub fn kv_out(map_accounts: Accounts, map_stat: Stats) -> Result<KvOperations, Error> {
-    let mut kv_ops: KvOperations = Default::default();
-
-    let mut ordinal = 1;
-    for account in map_accounts.items {
-        let asset = Asset::from(account.balance.as_str());
-        let key = format!(
-            "accounts:{}:{}:{}",
-            account.account, account.contract, asset.symbol
-        );
-        kv_ops.push_new(key, asset.amount.to_string(), ordinal);
-        ordinal += 1;
-    }
-
-    ordinal = 1;
-    for stat in map_stat.items {
-        let asset = Asset::from(stat.supply.as_str());
-        let key = format!("stat:{}:{}", stat.contract, asset.symbol);
-        kv_ops.push_new(key, asset.amount.to_string(), ordinal);
-        ordinal += 1;
-    }
-
-    Ok(kv_ops)
-}*/
-
-#[substreams::handlers::map]
-pub fn db_out(map_transfers: TransferEvents) -> Result<DatabaseChanges, Error> {
-    let mut db_out = DatabaseChanges::default();
-
-    for transfer in map_transfers.items {
-        let pk = format!("{}-{}", transfer.trx_id, transfer.action_ordinal);
-        db_out
-            .push_change("transfer", pk.as_str(), 0, table_change::Operation::Create)
-            // transaction
-            .change("trx_id", ("", transfer.trx_id.as_str()))
-            .change("action_ordinal", ("", transfer.action_ordinal.to_string().as_str()))
-
-            // contract & scope
-            .change("contract", ("", transfer.contract.as_str()))
-            .change("action", ("", transfer.action.as_str()))
-            .change("symcode", ("", transfer.symcode.as_str()))
-
-            // data payload
-            .change("from", ("", transfer.from.as_str()))
-            .change("to", ("", transfer.to.as_str()))
-            .change("memo", ("", transfer.memo.as_str()))
-            .change("quantity", ("", transfer.quantity.as_str()))
-
-            // extras
-            .change("amount", ("", transfer.amount.to_string().as_str()))
-            .change("precision", ("", transfer.precision.to_string().as_str()))
-            .change("value", ("", transfer.value.to_string().as_str()));
-    }
-
-    Ok(db_out)
+    Ok(prom_out)
 }
